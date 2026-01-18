de81abb1383f42bff98f27344f4d5f56ecc31bf4
jinsungkim <kimswim02@gmail.com>
2026-01-18 16:21:05 +0900

    fetch 적용

--- src/Main.js
+++ src/Main.js
@@ -3,16 +3,31 @@ import List from "./pages/List";
 import "./css/Main.css"
 
 function Main() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.bz/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
-        <div className="div-left">
-            <Calculator/>
-        </div>
-        <div className="div-right">
-            <List/>
-        </div>
+      <div className="div-left">
+        <Calculator />
+      </div>
+      <div className="div-right">
+        <List />
+      </div>
     </div>
-    
+
   );
 }
 
