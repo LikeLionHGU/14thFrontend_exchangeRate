797d84fb61117aa4f40fc4413d91ac3bd1c34dd1
cm030627 <cm030627@gmail.com>
2026-01-20 10:39:27 +0900

    style : 색상 추가 및 그리드 정렬

--- src/pages/List.js
+++ src/pages/List.js
@@ -3,14 +3,14 @@ import "../css/Main.css";
 
 function List() {
   const today = new Date();
-  const todayDate = `${today.getFullYear()}${(today.getMonth() + 1).toString().padStart(2, '0')}${today.getDate()}`;
+  const todayDate = `${today.getFullYear()}${(today.getMonth() + 1).toString().padStart(2, "0")}${today.getDate()}`;
 
   const [loading, setLoading] = useState(true);
   const [exchange, setExchange] = useState([]);
   const getExchange = async () => {
     const json = await (
       await fetch(
-        "https://api.allorigins.win/raw?url=https://ecos.bok.or.kr/api/StatisticSearch/GZOJK8A2NI5BBV6E0NIU/json/kr/1/43/731Y001/D/20260116/20260116"
+        `https://api.allorigins.win/raw?url=https://ecos.bok.or.kr/api/StatisticSearch/GZOJK8A2NI5BBV6E0NIU/json/kr/1/43/731Y001/D/${todayDate}/${todayDate}`,
       )
     ).json();
     setExchange(json.StatisticSearch.row);
@@ -19,26 +19,26 @@ function List() {
   useEffect(() => {
     getExchange();
   }, []);
-  console.log(exchange);
-  
+
   return (
     <div>
       {loading ? (
         <h1>loading</h1>
       ) : (
-        <div>
+        <div className="list">
           <h2>환율 리스트</h2>
-          {exchange.map((exchange) => (
-            <div className="list" key={exchange.ITEM_CODE1}>
-              <p>{exchange?.DATA_VALUE}원</p>
+          <div className="exchangeListMain">
+            {exchange.map((exchange) => (
+            <div className="exchangeListBox" key={exchange.ITEM_CODE1}>
+              <h2>{exchange?.DATA_VALUE}원</h2>
               <p>{exchange?.ITEM_NAME1}</p>
             </div>
-            
           ))}
+          </div>
+          
         </div>
       )}
     </div>
-    
   );
 }
 
