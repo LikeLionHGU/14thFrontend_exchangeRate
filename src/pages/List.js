5ffd797daedfcefe89eb3422b087d43551566e41
cm030627 <cm030627@gmail.com>
2026-01-21 01:29:58 +0900

    fix : 환율 변동이 없을 시 표시 오류 수정

--- src/pages/List.js
+++ src/pages/List.js
@@ -42,22 +42,40 @@ function List() {
               >
                 <h2>{parseFloat(todayExchange.DATA_VALUE)}원</h2>
 
-                {parseFloat(todayExchange.DATA_VALUE - yesterdayExchange[index].DATA_VALUE,).toFixed(1) >= 0 ? (
-                  <h3 style={{color: "red"}}>🔺
-                  {parseFloat(
+                {parseFloat(
+                  todayExchange.DATA_VALUE -
+                    yesterdayExchange[index].DATA_VALUE,
+                ).toFixed(2) == 0.0 ? (
+                  <h3>
+                    ➖
+                    {parseFloat(
+                      todayExchange.DATA_VALUE -
+                        yesterdayExchange[index].DATA_VALUE,
+                    ).toFixed(2)}
+                    원
+                  </h3>
+                ) : parseFloat(
                     todayExchange.DATA_VALUE -
-                      yesterdayExchange[index].DATA_VALUE).toFixed(2)}
-                  원
-                </h3>
+                      yesterdayExchange[index].DATA_VALUE,
+                  ).toFixed(2) >= 0.0 ? (
+                  <h3 style={{ color: "red" }}>
+                    🔺
+                    {parseFloat(
+                      todayExchange.DATA_VALUE -
+                        yesterdayExchange[index].DATA_VALUE,
+                    ).toFixed(2)}
+                    원
+                  </h3>
                 ) : (
-                  <h3 style={{color:"blue"}}>🔻
-                  {parseFloat(
-                    todayExchange.DATA_VALUE -
-                      yesterdayExchange[index].DATA_VALUE).toFixed(2)}
-                  원
-                </h3>
-                )
-                }
+                  <h3 style={{ color: "blue" }}>
+                    🔻
+                    {parseFloat(
+                      todayExchange.DATA_VALUE -
+                        yesterdayExchange[index].DATA_VALUE,
+                    ).toFixed(2)}
+                    원
+                  </h3>
+                )}
 
                 <p>{todayExchange.ITEM_NAME1.slice(2)}</p>
               </div>
