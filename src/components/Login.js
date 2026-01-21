08f5e896ad1589d335a12bda5c73828d4cd24c7a
cm030627 <cm030627@gmail.com>
2026-01-21 22:01:46 +0900

    test : 테스트

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
 const Login = async (username, password) => {
-  const response = await axios.post(
-    `${process.env.REACT_APP_HOST_URL}/Login`,
-    {
-      username,
-      password,
-    }
-  );
+  const response = await axios.post(`${process.env.REACT_APP_HOST_URL}/Login`, {
+    username,
+    password,
+  });
 
   return response.data;
 };
