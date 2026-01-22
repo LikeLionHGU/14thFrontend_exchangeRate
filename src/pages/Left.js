230f55e1d7cc521789ef668065901e659d2bffd0
cm030627 <cm030627@gmail.com>
2026-01-22 19:38:57 +0900

    [fix] - api 변경 및 경로 수정 완료

--- src/pages/Left.js
+++ src/pages/Left.js
@@ -1,32 +1,29 @@
 import List from "./List";
 import Calculator from "./Calculator";
 import "../css/Main.css";
-import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Submain() {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
-    const getExchange = async () => {
-        const response = await fetch(
-            `https://v6.exchangerate-api.com/v6/8be5102521075c8aee3cdb75/latest/USD`
-        );
-        const json = await response.json();
-        setData(json);
-    };
-    useEffect(
-        () => {
-            getExchange();
-            console.log(data);
-        }, []
+  const getExchange = async () => {
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/8be5102521075c8aee3cdb75/latest/USD`,
     );
-    return (
-        <div className="Msub-div">
-            <div className="gap">
-                <Calculator />
-                <List />
-            </div>
-        </div >
-    );
+    const json = await response.json();
+    setData(json.conversion_rates);
+  };
+  useEffect(() => {
+    getExchange();
+  }, []);
+
+  return (
+    <div className="Msub-div">
+      <div className="gap">
+        <Calculator />
+        <List data={data} />
+      </div>
+    </div>
+  );
 }
 export default Submain;
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,84 +1,23 @@
-import { useEffect, useState } from "react";
 import "../css/Main.css";
 import styles from "../css/List.module.css";
 
-function List() {
-  const today = new Date();
-  const todayDate = `${today.getFullYear()}${(today.getMonth() + 1).toString().padStart(2, "0")}${today.getDate()}`;
+function List({ data }) {
+  const list = Object.entries(data);
 
-  const [loading, setLoading] = useState(true);
-  const [todayExchange, setTodayExchange] = useState([]);
-  const [yesterdayExchange, setYesterdayExchange] = useState([]);
-  const getExchange = async () => {
-    const json1 = await (
-      await fetch(
-        `https://api.allorigins.win/raw?url=https://ecos.bok.or.kr/api/StatisticSearch/GZOJK8A2NI5BBV6E0NIU/json/kr/1/43/731Y001/D/20260120/20260120`,
-      )
-    ).json();
-    const json2 = await (
-      await fetch(
-        `https://api.allorigins.win/raw?url=https://ecos.bok.or.kr/api/StatisticSearch/GZOJK8A2NI5BBV6E0NIU/json/kr/1/43/731Y001/D/20260119/20260119`,
-      )
-    ).json();
-    setTodayExchange(json1.StatisticSearch.row);
-    setYesterdayExchange(json2.StatisticSearch.row);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getExchange();
-  }, []);
+  const won = data.KRW;
 
   return (
     <div>
-      {loading ? (
-        <h1>loading</h1>
-      ) : (
-        <div className={styles.list}>
-          <div className={styles.exchangeListMain}>
-            {todayExchange.map((todayExchange, index) => (
-              <div
-                className={styles.exchangeListBox}
-                key={todayExchange.ITEM_CODE1}
-              >
-                <h2>{parseFloat(todayExchange.DATA_VALUE)}원</h2>
-
-                {parseFloat(
-                  todayExchange.DATA_VALUE -
-                    yesterdayExchange[index].DATA_VALUE,
-                ).toFixed(2) == 0.0 ? (
-                  <h3>
-                    ➖
-                    0원
-                  </h3>
-                ) : parseFloat(
-                    todayExchange.DATA_VALUE -
-                      yesterdayExchange[index].DATA_VALUE,
-                  ).toFixed(2) >= 0.0 ? (
-                  <h3 style={{ color: "red" }}>
-                    🔺
-                    {parseFloat(
-                      todayExchange.DATA_VALUE -
-                        yesterdayExchange[index].DATA_VALUE,
-                    ).toFixed(2)}
-                    원
-                  </h3>
-                ) : (
-                  <h3 style={{ color: "blue" }}>
-                    🔻
-                    {parseFloat(
-                      todayExchange.DATA_VALUE -
-                        yesterdayExchange[index].DATA_VALUE,
-                    ).toFixed(2)}
-                    원
-                  </h3>
-                )}
-
-                <p>{todayExchange.ITEM_NAME1.slice(2)}</p>
-              </div>
-            ))}
-          </div>
+      <div className={styles.list}>
+        <div className={styles.exchangeListMain}>
+          {list.map((list) => (
+            <div className={styles.exchangeListBox} key={list[0]}>
+              <h1>{(won / list[1]).toFixed(2)}원</h1>
+              <h1>{list[0]} / 원</h1>
+            </div>
+          ))}
         </div>
-      )}
+      </div>
     </div>
   );
 }
